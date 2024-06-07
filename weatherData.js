4c5d6b130e51338bc8a357ae4cb1a8082a4d943b
nakamurakazuki <nakamura.kazuki@algoage.co.jp>
2024-06-08 00:19:17 +0900

    lintを実行した

--- weatherData.js
+++ weatherData.js
@@ -12,7 +12,9 @@ function readCSVFile(fileName) {
 
 export function getAvailablePrefectures(prefectures) {
   if (!fs.existsSync(csvDirectoryPath)) {
-    console.log("CSVディレクトリが存在しません。CSVディレクトリを配置してください。");
+    console.log(
+      "CSVディレクトリが存在しません。CSVディレクトリを配置してください。",
+    );
     process.exit(1);
   }
 
@@ -30,7 +32,14 @@ export function getAvailablePrefectures(prefectures) {
       if (csvArray[i]) {
         csvArray[i].forEach((element) => {
           prefectures.forEach((prefecture) => {
-            if (element.includes(prefecture.replace("県", "").replace("府", "").replace("都", ""))) {
+            if (
+              element.includes(
+                prefecture
+                  .replace("県", "")
+                  .replace("府", "")
+                  .replace("都", ""),
+              )
+            ) {
               availablePrefectures.add(prefecture);
             }
           });
@@ -47,7 +56,14 @@ function findCSVFilePath(prefecture) {
   return csvFiles.find((file) => {
     const csvArray = readCSVFile(file);
     for (let i = 0; i < 4; i++) {
-      if (csvArray[i] && csvArray[i].some((element) => element.includes(prefecture.replace("県", "").replace("府", "").replace("都", "")))) {
+      if (
+        csvArray[i] &&
+        csvArray[i].some((element) =>
+          element.includes(
+            prefecture.replace("県", "").replace("府", "").replace("都", ""),
+          ),
+        )
+      ) {
         return true;
       }
     }
@@ -95,10 +111,7 @@ function displayWeatherResult(mostCommonWeather, weatherCounts) {
   console.log("最も多い天候:", mostCommonWeather);
 
   const totalCount = Object.values(weatherCounts).reduce((a, b) => a + b, 0);
-  if (
-    mostCommonWeather === "雨" &&
-    weatherCounts["雨"] / totalCount > 1 / 3
-  ) {
+  if (mostCommonWeather === "雨" && weatherCounts["雨"] / totalCount > 1 / 3) {
     if (weatherCounts["雨"] > 10) {
       console.log("雨の確率が高いです");
     } else {
@@ -122,34 +135,37 @@ export function searchWeatherData(prefecture, rl) {
 
   const csvArray = readCSVFile(csvFilePath);
 
-  rl.question("検索する日付を入力してください（例: 1/1）\n入力しない場合は今日の日付になります: ", (searchDate) => {
-    if (!searchDate) {
-      const today = new Date();
-      const month = today.getMonth() + 1;
-      const day = today.getDate();
-      searchDate = `${month}/${day}`;
-    }
-
-    if (!searchDate.match(/^\d{1,2}\/\d{1,2}$/)) {
-      console.log("日付の形式が正しくありません。再度入力してください。");
-      searchWeatherData(prefecture, rl);
-      return;
-    }
-
-    const weatherCounts = getWeatherCounts(csvArray, searchDate);
-
-    let mostCommonWeather = "";
-    let highestCount = 0;
-
-    for (const [weather, count] of Object.entries(weatherCounts)) {
-      if (count > highestCount) {
-        mostCommonWeather = weather;
-        highestCount = count;
+  rl.question(
+    "検索する日付を入力してください（例: 1/1）\n入力しない場合は今日の日付になります: ",
+    (searchDate) => {
+      if (!searchDate) {
+        const today = new Date();
+        const month = today.getMonth() + 1;
+        const day = today.getDate();
+        searchDate = `${month}/${day}`;
       }
-    }
 
-    displayWeatherResult(mostCommonWeather, weatherCounts);
+      if (!searchDate.match(/^\d{1,2}\/\d{1,2}$/)) {
+        console.log("日付の形式が正しくありません。再度入力してください。");
+        searchWeatherData(prefecture, rl);
+        return;
+      }
 
-    rl.close();
-  });
+      const weatherCounts = getWeatherCounts(csvArray, searchDate);
+
+      let mostCommonWeather = "";
+      let highestCount = 0;
+
+      for (const [weather, count] of Object.entries(weatherCounts)) {
+        if (count > highestCount) {
+          mostCommonWeather = weather;
+          highestCount = count;
+        }
+      }
+
+      displayWeatherResult(mostCommonWeather, weatherCounts);
+
+      rl.close();
+    },
+  );
 }
